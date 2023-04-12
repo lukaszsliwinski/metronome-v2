70772d33ebcab5c95a943ca40e74ef87a255f98e
Łukasz Śliwiński <74149405+lukaszsliwinski@users.noreply.github.com>
2023-04-12 12:00:48 +0200

    fix: add missing quotes

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 // render react app
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/build', index.html));
+  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
 });
 
 // run server
